855dd78d94cbc9a037a90720f4568f5cbe2152f4
ARileyCS <157526404+ARileyCS@users.noreply.github.com>
2024-02-08 10:04:36 -0600

    Update main.py
    retry with all things in function and commented out input

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 #trying to go through, count how many of each character, and then compare that to the other list?????
 def anagram(word1, word2):
-    wcount = 0
+    
+    if (not is_valid_input(word1)) or (not is_valid_input(word2)):
+        return False
+    
+    ccount = 0
     angram = False
-
+    
     for character in word1:
         ccount = word1.count(character)
         if ccount == word2.count(character):
@@ -27,7 +31,7 @@ def is_valid_input(word):
 
 if __name__ == '__main__':
     #REMOVE PASS YOUR CODE GOES HERE
-    user_input1 = (input("Please enter one word: ").strip()).lower()
+    '''user_input1 = (input("Please enter one word: ").strip()).lower()
     user_input2 = (input("Please enter another word: ")).strip().lower()
 
 
@@ -37,4 +41,4 @@ if __name__ == '__main__':
 
     if is_valid_input(user_input1) and is_valid_input(user_input2):
         are_anagrams = anagram(user_input1, user_input2)
-        print(are_anagrams)
+        print(are_anagrams)'''
